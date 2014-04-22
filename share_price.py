0f1d2d97c04869ea40fcdc8814b1b0ac40f4e595
anshulyadav20 <rao786rao@gmail.com>
2014-04-22 11:15:53 +0530

    Create share_price.py

---
+++ share_price.py
@@ -0,0 +1,19 @@
+import csv
+from collections import OrderedDict, namedtuple
+
+with open('C:\Users\AOC2\Desktop\shareData_inconsistant.csv') as f:
+    reader = csv.reader(f)
+    tup = namedtuple('tup', ['price', 'year', 'month'])
+    d = OrderedDict()
+    names = next(reader)[2:]
+    for name in names:
+        #initialize the dict
+        d[name] = tup(0, 'year', 'month')
+    for row in reader:
+        year, month = row[:2]         # Use year, month, *prices = row in py3.x
+        for name, price in zip(names, map(int, row[2:])): # map(int, prices) py3.x
+            print name,price
+            print d[name].price
+            if d[name].price < price:
+                d[name] = tup(price, year, month)
+print d 
